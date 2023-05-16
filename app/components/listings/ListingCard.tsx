b3432e9c130173edf560e0188f089b4721960a9e
IgnacioLF <ignaciolopezfeliu@gmail.com>
2023-05-16 16:29:49 +0200

    -added trips screen

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import useCountries from "@/app/hooks/useCountries";
-import { SafeListings, SafeUser } from "@/app/types";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
@@ -10,9 +10,9 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: SafeListings;
-  reservation?: Reservation;
-  onAction?: (id: String) => void;
+  data: SafeListing;
+  reservation?: SafeReservation;
+  onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
   actionId?: string;
